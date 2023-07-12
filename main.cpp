a16ec26c81599b51234b877b7c6a43c4ea8068f5
cicer <jxkicker@163.com>
2023-07-12 23:12:06 +0800

    introduction

--- main.cpp
+++ main.cpp
@@ -9,7 +9,12 @@
 using namespace muse::pool;
 
 int main() {
-    ThreadPool<ThreadPoolType::Flexible, 1024, 8> pool(4, ThreadPoolCloseStrategy::WaitAllTaskFinish);
+    //创建一个线程池
+    //队列最大程度为 1024
+    //线程数量可以动态变化，最多线程数量为 8，最少为 4
+    //线程关闭策略为 等待所有任务执行完毕后才关闭
+    //线程池 管理线程运行频率为 1.9s
+    ThreadPool<ThreadPoolType::Flexible, 1024, 8> pool(4, ThreadPoolCloseStrategy::WaitAllTaskFinish, 1900ms);
 
     //将任务包装成一个执行器
     auto executor = make_executor([](int i)->int{
@@ -26,6 +31,7 @@ int main() {
             std::cout << "result :" << executor->get() << std::endl;
         }
     }
-
+    //关闭线程池
+    pool.close();
     return 0;
 }
